d4757fd47244031677d590f761e250ebdf86d587
sivaprakash9342 <sivaprakash934278@gmail.com>
2026-08-27 10:01:47 +0530

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -1,11 +1,8 @@
--- CollegeDB SQL Assignment
--- Student Name:
--- Register Number:
-
--- Write SQL commands below
-
--- 1. Create Database
-
--- 2. Select Database
-
--- 3. Create Department Table
+USE CollegeDB;
+CREATE TABLE Departments
+(
+DepartmentID INT(5) PRIMARY KEY,
+DepartmentName VARCHAR(20),
+HOD VARCHAR(20)
+);
+DESC Departments;
